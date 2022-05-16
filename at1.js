8f13bf7396337c03a14f31a42612ad7bdf0ec8a0
dybeca123 <103453892+dybeca123@users.noreply.github.com>
2022-05-16 14:28:24 -0300

    Add files via upload

---
+++ at1.js
@@ -0,0 +1,31 @@
+//at'
+
+let n = 1
+while (n <= 100){
+console.log (n)
+n = n + 1 
+}
+
+//at2
+
+let o = 50
+while (o <= 100){
+    console.log (n)
+    o + o + 1
+}
+
+//at3
+
+let p = 10
+while ( p >= 0){
+    console.log (n)
+    p = p - 1
+}
+
+//at4
+
+let q = 0
+while (q <= 20 ){
+    console.log (n)
+    q = q + 2
+}
